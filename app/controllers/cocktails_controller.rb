993979c0c631ef3e9679e7a6fb3a91d9df2a064d
Jad Azzam <jadazzam@me.com>
2019-02-17 22:08:37 +0100

    data from home => button => show

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -24,7 +24,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :photo)
+    params.require(:cocktail).permit(:name, :body, :photo)
   end
 
 end
